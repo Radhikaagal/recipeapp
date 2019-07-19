27f10c8aeed74120d88b4d7c1045a47b7f7a5f71
Radhikaagal <radhika.agal@gmail.com>
2019-07-19 10:41:58 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import Search from './components/Search';
+import Recipes from './components/Recipes';
 import './App.css';
+const YOUR_API_KEY="45aa8bc57206c09fd1a7db1be8b91f48";
+class App extends React.Component {
+  
+  constructor(props)
+  {
+    super(props);
+    this.searchRecipe=this.searchRecipe.bind(this);
+    this.state={
+      recipes: []
+    }
+  }
 
-function App() {
-  return (
+searchRecipe(recipename)
+{
+  fetch(`https://www.food2fork.com/api/search?key=${YOUR_API_KEY}&q=${recipename}`)
+  .then(response=>response.json())
+  //.then(data=>console.log("fectched json data",data))
+  .then(data=>this.setState({
+    recipes: data.recipes
+  }))
+  
+  // 
+}
+
+render()
+{
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Recipe</h1>
       </header>
+      <Search onSubmit={this.searchRecipe} />
+      <br />
+      <Recipes recipes={this.state.recipes}/>
     </div>
   );
 }
-
+}
 export default App;
